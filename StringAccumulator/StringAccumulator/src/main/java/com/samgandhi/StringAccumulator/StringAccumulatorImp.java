a300d38471fb383ca826fdab2eecceceb6f3ec60
sameergandhi <sameergandhi.it@gmail.com>
2018-09-22 18:32:29 +0800

    String Accumulator - A Test Program

---
+++ StringAccumulator/StringAccumulator/src/main/java/com/samgandhi/StringAccumulator/StringAccumulatorImp.java
@@ -0,0 +1,83 @@
+package com.samgandhi.StringAccumulator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StringAccumulatorImp implements StringAccumulator {
+
+	private ArrayList<Integer> negativeArray = null;
+
+	@Override
+	public int add(String numbers) throws Exception {
+		String negativeList = "";
+		List<String> delimiters = getdelimiters(numbers);
+		if (numbers.substring(0, 2).equals("//")) {
+			numbers = numbers.substring(numbers.indexOf("\n") + 1);
+		}
+		for (int i = 0; i < delimiters.size(); i++) {
+			numbers = numbers.replaceAll(Pattern.quote(delimiters.get(i)), StringAccumulator.DEFAULT_DELIMITER);
+		}
+		negativeArray = new ArrayList<Integer>();
+		List<Integer> list = parseIntegerNumber(numbers, StringAccumulator.DEFAULT_DELIMITER);
+		Optional<Integer> sum = list.stream().filter(s -> s.intValue() < StringAccumulator.MAX_ADD_NUMBER)
+				.reduce(Integer::sum);
+		if (negativeArray.size() > 0) {
+			for (int negative : negativeArray) {
+				negativeList = negativeList.equals("") ? negativeList : negativeList + ", ";
+				negativeList += negative;
+			}
+			throw new Exception("negatives not allowed : " + negativeList);
+		} else if (list.size() <= 1) {
+			throw new Exception("Invalid Input..");
+		} else {
+			return sum.get();
+		}
+	}
+
+	@Override
+	public ArrayList<String> getdelimiters(String numbers) {
+		ArrayList<String> delimitorPatterns = new ArrayList<String>();
+		if (numbers.substring(0, 2).equals("//")) {
+			numbers = numbers.substring(2);
+			delimitorPatterns = (ArrayList<String>) Arrays
+					.asList(numbers.substring(0, numbers.indexOf("\n")).split(Pattern.quote("|"))).stream().distinct()
+					.map(Object::toString).collect(Collectors.toList());
+			return delimitorPatterns;
+		}
+
+		if (!delimitorPatterns.contains(",")) {
+			delimitorPatterns.add(",");
+		}
+		return delimitorPatterns;
+	}
+
+	@Override
+	public List<Integer> parseIntegerNumber(String number, String delimator) throws Exception {
+		try {
+			int[] numericList = Stream.of(number.split(delimator)).mapToInt(Integer::parseInt)
+					.peek(n -> getNegativeNumbers(n)).peek(s -> {
+						if ("".equals(s))
+							throw new IllegalArgumentException(String.valueOf(s));
+					}).toArray();
+			return Arrays.stream(numericList).boxed().collect(Collectors.toList());
+		} catch (Exception e) {
+			System.out.println("Exception thrown :" + e.getLocalizedMessage());
+		}
+		return null;
+	}
+
+	private Integer getNegativeNumbers(Integer value) {
+		if (value < 0) {
+			negativeArray.add(value);
+			return 0;
+		} else {
+			return value;
+		}
+	}
+
+}
---
+++ StringAccumulator/StringAccumulator/src/test/java/com/samgandhi/StringAccumulator/StringAccumulatorTest.java
@@ -0,0 +1,47 @@
+package com.samgandhi.StringAccumulator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junit.framework.TestCase;
+
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class StringAccumulatorTest extends TestCase {
+
+	private String str;
+	private int expectedResult;
+	private StringAccumulatorImp service;
+
+	@Before
+	public void initialize() {
+		service = new StringAccumulatorImp();
+	}
+
+	public StringAccumulatorTest(String str, int expectedResult) {
+		this.str = str;
+		this.expectedResult = expectedResult;
+	}
+
+	@Parameterized.Parameters
+	public static Collection inputStrings() {
+		return (Collection) Arrays.asList(new Object[][] { { "//**|,|^\n2**1001,23\n34\n", 59 },
+			{ "//**|,|^\n2**1000,23\n34\n", 1059 }, { "//**|,|^\n23\n34\n", 57 },
+			{ "//**|,|^\n2**-1001,\n-23\n-34\n", 2 }, { "//**|,|^\n23,\n", 23 }, { "//**|,|^\n23,23", 46 } });
+	}
+
+	@Test
+	public void testStrinAccumulatorChecker() {
+		System.out.println("Input string is : " + str);
+		try {
+			assertEquals(expectedResult, service.add(str));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
