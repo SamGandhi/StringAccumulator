6718d2ea29fb0cfd67eefc98c5871fba667fc483
SamGandhi <43477199+SamGandhi@users.noreply.github.com>
2018-09-24 00:56:46 +0800

    Update StringAccumulatorService.java

--- StringAccumulator/src/main/java/com/samgandhi/StringAccumulator/StringAccumulatorService.java
+++ StringAccumulator/src/main/java/com/samgandhi/StringAccumulator/StringAccumulatorService.java
@@ -3,7 +3,7 @@ package com.samgandhi.StringAccumulator;
 public class StringAccumulatorService {
 
 	public static void main(String argv[]) {
-		System.out.println("hello world");
+		System.out.println("Welcome to String Accumulator Service..");
 		StringAccumulatorImp service = new StringAccumulatorImp();
 		try {
 			System.out.println(service.add("1,2,3,4,5,6,7,89,90,26,78,57,89"));
